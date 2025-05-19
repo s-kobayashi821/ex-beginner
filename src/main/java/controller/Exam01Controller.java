b73880d15cada7fd404b3909ed73aa788c61e938
S.Kobayashi <shunsuke.kobayashi@rakus.co.jp>
2025-05-19 13:56:59 +0900

    Exam01Controller.java の追加

---
+++ src/main/java/controller/Exam01Controller.java
@@ -0,0 +1,21 @@
+package controller;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@org.springframework.stereotype.Controller
+@Repository("exam01")
+public class Exam01Controller {
+
+    @GetMapping("")
+    public String index(Model model){
+        return "exam01";
+    }
+
+    @PostMapping("/result")
+    public String result(String name, Model model){
+        return "result";
+    }
+}

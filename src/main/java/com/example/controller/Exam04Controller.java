96a3d337eb597545016da6499f818dce2af504b6
S.Kobayashi <shunsuke.kobayashi@rakus.co.jp>
2025-05-19 15:47:28 +0900

    Validaterを実装

---
+++ src/main/java/com/example/controller/Exam04Controller.java
@@ -0,0 +1,37 @@
+package com.example.controller;
+
+import com.example.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/exam04")
+public class Exam04Controller {
+
+    @GetMapping("")
+    public String index(UserForm userForm){
+        return "exam04";
+    }
+
+    @PostMapping("/result")
+    public String result(@Validated UserForm userForm,
+                        BindingResult result,
+                         Model model){
+
+        if (result.hasErrors()){
+            return index(userForm);
+        }
+
+        User user = new User();
+        user.setName(userForm.getName());
+        user.setAge(userForm.getAge());
+        user.setComment(userForm.getComment());
+        model.addAttribute("user", user);
+    }
+
+}
---
+++ src/main/java/com/example/controller/UserForm.java
@@ -0,0 +1,43 @@
+package com.example.controller;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class UserForm {
+
+    @NotBlank(message = "名前は必須です")
+    private String name;
+
+    @NotNull(message = "年齢を入力してください")
+    private Integer age;
+
+    @Size(min = 1, message = "コメントを入力してください")
+    private String comment;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+
+}

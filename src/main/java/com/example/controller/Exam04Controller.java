6915504394625cb3b23f3b2807a236f65b497895
S.Kobayashi <shunsuke.kobayashi@rakus.co.jp>
2025-05-19 15:37:36 +0900

    Validationを追加

--- src/main/java/com/example/controller/Exam04Controller.java
+++ src/main/java/com/example/controller/Exam04Controller.java
@@ -3,6 +3,8 @@ package com.example.controller;
 import com.example.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +14,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Exam04Controller {
 
     @GetMapping("")
-    public String index(UserForm userForm){
+    public String index(UserForm userForm) {
         return "exam04";
     }
 
     @PostMapping("/result")
-    public String result(UserForm userForm, Model model){
+    public String result(
+            @Validated UserForm userForm,
+            BindingResult result,
+            Model model) {
+
+        if (result.hasErrors()) {
+            return index(userForm);
+        }
+
         User user = new User();
         user.setName(userForm.getName());
         user.setAge(userForm.getAge());
--- src/main/java/com/example/controller/UserForm.java
+++ src/main/java/com/example/controller/UserForm.java
@@ -1,8 +1,18 @@
 package com.example.controller;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class UserForm {
+    @NotBlank(message="名前は必須です")
     private String name;
+
+    @NotNull(message = "年齢を入力してください")
     private Integer age;
+
+    @Size(min=1, max = 127, message = "1文字以上のコメントをお願いします。")
     private String comment;
 
     public String getName() {
